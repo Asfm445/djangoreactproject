8bc8f10411e6789d061a933f3f288f9b0233584c
Asfm445 <awelabubekar@gmail.com>
2025-02-01 01:19:12 -0800

    latest commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#3cpt6ul^2g8npulhtxezgiurw-wx&4-dg@-u2!8c&a()so(qe"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 REST_FRAMEWORK = {
